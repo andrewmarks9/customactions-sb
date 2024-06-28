1590314634c6d599ac365713cc68b862b80c4c4f
amarks916 <andrewlmarks916@gmail.com>
2024-06-28 15:58:26 -0400

    fix main.js

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,18 +1,25 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
-function run () {
+async function run() {
     // Get input values
     const bucket = core.getInput('bucket', {required: true});
     const bucketRegion = core.getInput('bucket-region', {required: false});
     const distFolder = core.getInput('dist-folder', {required: true});
-    
 
-    // upload files to S3
-    const s3Uri = `s3://${bucket}`;
-    exec.exec(`aws s3 sync ${distFolder} ${s3Uri}`, { region: `${bucketRegion}` });
+    // Construct the AWS CLI command
+    let command = `aws s3 sync ${distFolder} s3://${bucket}`;
+    if (bucketRegion) {
+        command += ` --region ${bucketRegion}`;
+    }
 
-    core.notice('Hello from the deploy-s3-javascript action!');
+    // Upload files to S3
+    try {
+        await exec.exec(command);
+        core.notice('Hello from the deploy-s3-javascript action!');
+    } catch (error) {
+        core.setFailed(`Action failed with error: ${error}`);
+    }
 }
 
-run()
+run();
