dcc3d54f2a345951c5f1310a3b41032325487270
amarks916 <andrewlmarks916@gmail.com>
2024-06-28 15:52:35 -0400

    added custom s3 upload

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,18 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run () {
+    // Get input values
+    const bucket = core.getInput('bucket', {required: true});
+    const bucketRegion = core.getInput('bucket-region', {required: false});
+    const distFolder = core.getInput('dist-folder', {required: true});
+    
+
+    // upload files to S3
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri}` --region ${bucketRegion}`);
+
     core.notice('Hello from the deploy-s3-javascript action!');
 }
 
